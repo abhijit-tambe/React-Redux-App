67a11ce2aceaa5bab276e040bfa8d70a7f98fc5e
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-27 14:23:59 -0400

    Update rootReducer.js

--- react-redux-app/src/rootReducer.js
+++ react-redux-app/src/rootReducer.js
@@ -1,18 +1,70 @@
-const initialState = {
-  counter: 0,
-};
+import { combineReducers } from "redux";
 
-function rootReducer(state = initialState, action) {
+// store elements
+const initialCake = {
+  cakeCount: 20,
+};
+const initialCookies = {
+  cookiesCount: 50,
+};
+// store elements
+
+// defining type
+export const BUY_CAKE = "buyCake";
+export const BUY_COOKIES = "buyCookies";
+// defining type
+
+//actions functions to return action objects
+export function buyCake() {
+  return {
+    type: BUY_CAKE,
+  };
+}
+
+export function buyCookies() {
+  return {
+    type: BUY_COOKIES,
+  };
+}
+//actions functions to return action objects
+
+// reducers
+export function cakeReducer(state = initialCake, action) {
   switch (action.type) {
-    case "increment":
+    case BUY_CAKE:
       return {
-        counter: state.counter + 1,
+        cakeCount: state.cakeCount - 1,
       };
-    case "decrement":
-      return {
-        counter: state.counter - 1,
-      };
+    // case "decrement":
+    //   return {
+    //     counter: state.counter - 1,
+    //   };
     default:
       return state;
   }
 }
+
+export function cookiesReducer(state = initialCookies, action) {
+  switch (action.type) {
+    case BUY_COOKIES:
+      return {
+        cookiesCount: state.cookiesCount - 1,
+      };
+    // case "decrement":
+    //   return {
+    //     counter: state.counter - 1,
+    //   };
+    default:
+      return state;
+  }
+}
+//reducers
+
+//combine reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  cookies: cookiesReducer,
+});
+//combine reducers
+
+export default rootReducer;
